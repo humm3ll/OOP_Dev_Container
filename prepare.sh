121295b0c75d08081d63253a79c371154aacba78
Dialexy <filiperamos212@gmail.com>
2025-11-19 15:18:48 +0000

    minor changes :)

---
+++ prepare.sh
@@ -0,0 +1,10 @@
+
+#!/usr/bin/env bash
+
+# echo "Creating Module Directory Structure"
+# /bin/bash -c "mkdir -p tutorials/{week01,week02,week03,week04,week05,week06,week07,week08,week09,week10,week11,week12} tasks/{task01,task02,task03,task04,task05}"
+
+
+/bin/bash -c "rm echo"
+exec "$@"
+
